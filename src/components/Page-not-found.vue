22545c79d20c2eb6cf03930902e4473530563642
yeasir-hossain <yeasir06@gmail.com>
2024-10-21 13:46:51 +0600

    feat: loading and error page

--- src/components/Page-not-found.vue
+++ src/components/Page-not-found.vue
@@ -7,7 +7,7 @@ import { RouterLink } from 'vue-router';
     <div class="text-center">
       <h1 class="text-6xl font-bold text-indigo-500 mb-4">404</h1>
       <p class="text-xl mb-6 text-gray-600">Oops! The page you're looking for doesn't exist.</p>
-      <RouterLink to="/" class="btn">
+      <RouterLink to="/" class="btn inline-flex">
         Go to Home
       </RouterLink>
     </div>
